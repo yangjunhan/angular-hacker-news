8af0d0dac9f61f3f083019068a479f84c7fe64d9
Junhan Yang <yangjunhan1232@sina.com>
2020-03-02 14:48:08 +0800

    Junhan: add footer and header using flexible box

--- angular-hacker-news/src/app/app.module.ts
+++ angular-hacker-news/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NewsListComponent } from './components/news-list/news-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NewsListComponent
   ],
   imports: [
     BrowserModule,
