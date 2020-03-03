4b7b740699a4bcba1c69585654cefe36b004bbeb
Junhan Yang <yangjunhan1232@sina.com>
2020-03-03 15:21:10 +0800

    Junhan: improve header and define ngOnInit() for NewsListComponent
    In the ngOnInit(), attempt to query the category for displaying in the queryParams. The category value obtained will be used for later tasks.

--- angular-hacker-news/src/app/components/news-list/news-list.component.ts
+++ angular-hacker-news/src/app/components/news-list/news-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-news-list',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./news-list.component.css']
 })
 export class NewsListComponent implements OnInit {
-
-  constructor() { }
+  // set default news category as 'top'
+  default = 'top';
+  category: string;
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    // obtain current category for displaying
+    this.route.queryParams.subscribe(params => {
+      // ensure that current query parameter 'category' is not undefined
+      if (typeof params.category === 'string') {
+        this.category = params.category;
+      } else {
+        this.category = this.default;
+      }
+      console.log('Current category is ' + this.category);
+    });
   }
 
 }
