e46723fbc6a821f505c070c84200d58134bc1585
Junhan Yang <yangjunhan1232@sina.com>
2020-03-05 15:57:29 +0800

    feat(core): implement pagination feature in the main component

--- angular-hacker-news/src/app/components/news-list/news-list.component.ts
+++ angular-hacker-news/src/app/components/news-list/news-list.component.ts
@@ -1,20 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { HackerNewsApiService } from '../../services/hacker-news-api.service';
-import { Observable } from 'rxjs';
-import {map} from "rxjs/operators";
+import {Observable, Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-news-list',
   templateUrl: './news-list.component.html',
   styleUrls: ['./news-list.component.css']
 })
-export class NewsListComponent implements OnInit {
+export class NewsListComponent implements OnInit, OnDestroy {
   // set default news category as 'topstories'
   private default = 'topstories';
   private newsItems$: Observable<object>;
+  private subscription: Subscription;
   public category: string;
   public newsItems: Array<object>;
+  public pageNum: number;
+  public totalPage: number;
+  public pageSize: number;
 
   constructor(
     private route: ActivatedRoute,
@@ -22,10 +25,11 @@ export class NewsListComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
+    this.pageSize = 20;
     // obtain current category for displaying
     this.route.queryParams.subscribe(params => {
-      // clear previous news items
-      this.newsItems = [];
+      // refresh page number
+      this.pageNum = 1;
       // ensure that current query parameter 'category' is not undefined
       if (typeof params.category === 'string') {
         this.category = params.category;
@@ -33,15 +37,40 @@ export class NewsListComponent implements OnInit {
         this.category = this.default;
       }
       console.log('Current category is ' + this.category);
-      this.newsItems$ = this.api.getNewsForPage(this.category, 1, 20);
-      this.newsItems$.subscribe(
-        data => {
-          console.log(data);
-          this.newsItems.push(data);
-        }, error => {
-          console.log(error);
-      });
+      this.getNewsForCurrentPage();
     });
   }
 
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
+  prevPage() {
+    this.pageNum = this.pageNum === 1 ? 1 : this.pageNum -= 1;
+    this.getNewsForCurrentPage();
+  }
+
+  nextPage() {
+    this.pageNum = this.pageNum === this.totalPage ? this.totalPage : this.pageNum += 1;
+    this.getNewsForCurrentPage();
+  }
+
+  getNewsForCurrentPage() {
+    // unsubscribe previous subscription if exists
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+    // clear previous news items
+    this.newsItems = [];
+    // load news items
+    this.newsItems$ = this.api.getNewsForPage(this.category, this.pageNum, this.pageSize);
+    this.subscription = this.newsItems$.subscribe(
+      data => {
+        console.log(data);
+        this.totalPage = Math.ceil(this.api.dataSize / this.pageSize);
+        this.newsItems.push(data);
+      }, error => {
+        console.log(error);
+      });
+  }
 }
--- angular-hacker-news/src/app/services/hacker-news-api.service.ts
+++ angular-hacker-news/src/app/services/hacker-news-api.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import {from, Observable, of} from 'rxjs';
-import {retry, map, catchError, flatMap} from 'rxjs/operators';
+import {retry, map, tap, catchError, flatMap} from 'rxjs/operators';
 
 const prefix = 'https://hacker-news.firebaseio.com/v0/';
 
@@ -10,13 +10,20 @@ const prefix = 'https://hacker-news.firebaseio.com/v0/';
 })
 export class HackerNewsApiService {
   private reqUrl: string;
+  private dataSource: Observable<object>;
+  public dataSize: number;
+
   constructor(private http: HttpClient) { }
 
   getNewsByCategory(category): Observable<any> {
     this.reqUrl = prefix + category + '.json?print=pretty';
     console.log(this.reqUrl);
     return this.http.get(this.reqUrl)
-      .pipe(retry(3));
+      .pipe(retry(3))
+      .pipe(tap(data => {
+        this.dataSource = data;
+        this.dataSize = data.length;
+      }));
   }
 
   getNewsItemById(id) {
